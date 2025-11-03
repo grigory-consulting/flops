1ce817d07c8740cc0a50a08c7507ed50b51c9874
grigory-consulting <consulting@vadze.de>
2025-11-03 15:34:23 +0100

    Update linear_models.py

--- linear_models.py
+++ linear_models.py
@@ -5,3 +5,28 @@ plt.xlabel("x")
 plt.ylabel("y")
 plt.axis([0,2,0,15])
 plt.show()
+
+
+
+
+
+
+
+
+
+n_epochs = 50 
+batch_size = 10
+m = X.shape[0]
+eta = 0.01 
+w_mbgd = np.random.randn(2,1) 
+
+for epoch in range(n_epochs): 
+    shuffled_indices = np.random.permutation(m)
+    X_b_shuffled = X_b[shuffled_indices] 
+    y_shuffled = y[shuffled_indices]
+    for i in range(0, m, batch_size):
+        xi = X_b_shuffled[i:i+batch_size]
+        yi = y_shuffled[i:i + batch_size]
+        gradients = 2/batch_size * xi.T @ (xi @ w_mbgd - yi) 
+        w_mbgd = w_mbgd - eta*gradients                      
+w_mbgd
