b71f6d7a6213ad37d3ccc5778da1560d7584fbad
grigory-consulting <consulting@vadze.de>
2025-11-04 09:12:49 +0100

    Create fashion.py

---
+++ fashion.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim 
+from torchvision import datasets, transforms
+import pandas as pd
+
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+
+
+train_dataset = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
