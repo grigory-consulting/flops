edc1ac3339a36f104c053d12e71d29329933fac6
grigory-consulting <consulting@vadze.de>
2025-11-04 10:17:40 +0100

    Update fashion.py

--- fashion.py
+++ fashion.py
@@ -16,3 +16,23 @@ test_dataset = datasets.FashionMNIST(root='./data', train=False, download=True, 
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+
+
+
+
+
+model.eval() 
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for images, labels in test_loader:
+            outputs = model(images)
+            predicted = torch.max(outputs.data, 1)[-1] 
+            total += labels.size(0) 
+            correct += (predicted==labels).sum().item()
+
+    accuracy = correct/total
+    print(f"Accuracy: {accuracy*100:.2f}%")
+    mlflow.log_metric("accuracy", accuracy)
