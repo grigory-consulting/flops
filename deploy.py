a97d6286bbe85676e9dd3c85ef51dddca1a42216
grigory-consulting <consulting@vadze.de>
2025-11-05 15:52:39 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -6,3 +6,20 @@ model_version = 1
 
 model_uri = f"models:/{model_name}/{model_version}"
 model = mlflow.pytorch.load_model(model_uri)
+
+
+model = mlflow.pytorch.load_model(model_uri)
+example_input = torch.randn(1, 3, 32, 32)
+
+model.eval()
+with torch.no_grad():
+    output = model(example_input)
+
+print(f"Raw: {output}")
+probs = F.softmax(output, dim =1 )
+
+print(f"Probabilities {probs*100}")
+
+predicted = torch.argmax(probs, dim = 1)
+
+print(f"Prediction: {predicted.item()}" )
