b1f373f8ce671515b36c7d56cbaae0a10e878674
grigory-consulting <consulting@vadze.de>
2025-11-05 13:52:41 +0100

    Update cifar-5.py

--- cifar-5.py
+++ cifar-5.py
@@ -104,5 +104,17 @@ with mlflow.start_run() as run:
 
 
 
+import mlflow.pytorch
+import torch.nn.functional as F
+
+model_name = "Best_CNN"
+model_version = 1
+
+model_uri = f"models:/{model_name}/{model_version}"
+model = mlflow.pytorch.load_model(model_uri)
+
+
+
+
 
 
