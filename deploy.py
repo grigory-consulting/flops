7eb0f550f9f3ae91039a126093c66dde74cad341
grigory-consulting <consulting@vadze.de>
2025-11-05 15:56:59 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -23,3 +23,27 @@ print(f"Probabilities {probs*100}")
 predicted = torch.argmax(probs, dim = 1)
 
 print(f"Prediction: {predicted.item()}" )
+
+
+
+
+
+
+
+
+
+import requests
+import torch
+import json
+import numpy as np
+
+# API URL des Modell-Servers
+url = "http://127.0.0.1:5007/invocations"
+
+example_input = torch.randn(1, 3, 32, 32).numpy().tolist()  # Modell erwartet NumPy-Array
+
+input_data = json.dumps({"instances": example_input})
+
+response = requests.post(url, data=input_data, headers={"Content-Type": "application/json"})
+
+print("Modell-Inferenz Ergebnis:", F.softmax(torch.tensor(response.json()["predictions"][0]), dim = 0))
